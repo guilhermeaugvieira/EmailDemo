52cf0da0d226589e7fe4aa6f8d74cdb7c4c912ad
Guilherme Augusto Vieira <guilhermeavdino@gmail.com>
2021-12-27 13:46:35 -0300

    Email Configuration
    DotNet Email Configuration

---
+++ src/EmailDemo.Application/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FluentEmail.Core;
+using FluentEmail.Smtp;
+using System.Net;
+using System.Net.Mail;
+
+Console.WriteLine("Hello, World!");
+
+var sender = new SmtpSender(() => new SmtpClient("smtp.mailtrap.io", 2525)
+{
+    Credentials = new NetworkCredential("1ccd533fe6bf6f", "09935031c06ac7"),
+    EnableSsl = true
+});
+
+Email.DefaultSender = sender;
+
+var email = await Email
+    .From("from@example.com")
+    .To("to@example.com")
+    .Subject("Hello World")
+    .Body("Test Body")
+    .SendAsync();
